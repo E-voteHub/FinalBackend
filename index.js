47f340e4d1b8725cc083d5a3dec93265e2549f8b
Anshu singh <idontknowanything144@gmail.com>
2024-12-05 05:04:24 +0530

    vercel.json

--- index.js
+++ index.js
@@ -27,17 +27,10 @@ dotenv.config()
 app.use(cors({
   origin: '*',  // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
-  credentials: true, // Allow credentials (cookies, etc.)
+  allowedHeaders: ['Content-Type', 'Authorization'],
+ 
 }));
 
-// Explicitly Handle Preflight OPTIONS Requests
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-csrf-token');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200); // Return a successful response for preflight
-});
 
 const app = express();
 const PORT = process.env.PORT || 3000
